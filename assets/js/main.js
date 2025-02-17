b46f43dc71742e5beb22790a8c11027f7a1ece38
Samuel Forrest <samueljforrest@outlook.com>
2025-02-17 17:25:45 +0000

    remove typing effect from main.js and clean up code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/* ----- NAVIGATION BAR FUNCTION ----- */
+/* NAVIGATION BAR FUNCTION */
 function myMenuFunction(){
   var menuBtn = document.getElementById("myNavMenu");
   if(menuBtn.className === "nav-menu"){
@@ -7,7 +7,7 @@ function myMenuFunction(){
     menuBtn.className = "nav-menu";
   }
 }
-/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+/* ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING */
 window.onscroll = function() {headerShadow()};
 function headerShadow() {
   const navHeader =document.getElementById("header");
@@ -21,22 +21,15 @@ function headerShadow() {
     navHeader.style.lineHeight = "90px";
   }
 }
-/* ----- TYPING EFFECT ----- */
-var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
-  loop : true,
-  typeSpeed : 100, 
-  backSpeed : 80,
-  backDelay : 2000
-})
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+
+/* SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
       origin: 'top',
       distance: '80px',
       duration: 2000,
       reset: true     
 })
-/* -- HOME -- */
+
 sr.reveal('.featured-text-card',{})
 sr.reveal('.featured-name',{delay: 100})
 sr.reveal('.featured-text-info',{delay: 200})
@@ -44,12 +37,13 @@ sr.reveal('.featured-text-btn',{delay: 200})
 sr.reveal('.social_icons',{delay: 200})
 sr.reveal('.featured-image',{delay: 300})
 
-/* -- PROJECT BOX -- */
+/* PROJECT BOX */
 sr.reveal('.project-box',{interval: 200})
-/* -- HEADINGS -- */
+
 sr.reveal('.top-header',{})
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-/* -- ABOUT INFO & CONTACT INFO -- */
+
+
+/* ABOUT INFO & CONTACT INFO  */
 const srLeft = ScrollReveal({
 origin: 'left',
 distance: '80px',
@@ -59,7 +53,9 @@ reset: true
 
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
-/* -- ABOUT SKILLS & FORM BOX -- */
+
+/* ABOUT SKILLS & FORM BOX  */
+
 const srRight = ScrollReveal({
 origin: 'right',
 distance: '80px',
@@ -87,6 +83,7 @@ sections.forEach(current =>{
 })
 }
 window.addEventListener('scroll', scrollActive)
+
 /* JAVASCRIPT FOR CONTACT FORM */
 
 function sendEmail() {
